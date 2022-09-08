2cf464ec9d37911e7a185e3aec67fa488edc7b62
Todd Whitten <twhitten@kennesaw.edu>
2022-09-08 12:04:08 -0400

    tailwind baseline

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,4 +1,43 @@
 <template>
+  <ul class="divide-y divide-gray-200">
+    <li v-for="person in people" :key="person.email" class="py-4 flex">
+      <img class="h-10 w-10 rounded-full" :src="person.image" alt="" />
+      <div class="ml-3">
+        <p class="text-sm font-medium text-gray-900">{{ person.name }}</p>
+        <p class="text-sm text-gray-500">{{ person.email }}</p>
+      </div>
+    </li>
+  </ul>
+</template>
+
+<script>
+const people = [
+  {
+    name: 'Todd Whitten',
+    email: 'todd.ryan.whitten@gmail.com',
+    image:
+      'https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+  },
+  {
+    name: 'Will Marsh Whitten',
+    email: 'will.marsh.whitten@gmail.com',
+    image:
+      'https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+  },
+]
+
+export default {
+  setup() {
+    return {
+      people,
+    }
+  },
+}
+</script>
+
+
+
+<!-- <template>
   <img src="../assets/shannon_nyc.jpg" />
   <div class="hello">
     <h1>{{ msg }}</h1>
@@ -12,4 +51,4 @@ export default {
     msg: String
   }
 }
-</script>
+</script> -->
