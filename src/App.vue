040de7e5f745cc2ba83cbeb5495009ba47cf7f54
Todd Whitten <todd.ryan.whitten@gmail.com>
2023-10-16 07:32:45 -0400

    add wmw pics

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,14 @@
 <template>
   <IndexMenu menuItem="Do what you can, with what you have, where you are"></IndexMenu>
-  <HelloWorld msg="2023/09/22"/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 import IndexMenu from './components/IndexMenu.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld,
-    IndexMenu
+    IndexMenu,
   }
 }
 </script>
--- src/components/IndexMenu.vue
+++ src/components/IndexMenu.vue
@@ -2,7 +2,12 @@
       <div class="indexMenu">
         <h4>{{ menuItem }}</h4>
       </div>
-  </template>
+      <swiper-container slides-per-view="3" speed="500" loop="true" css-mode="true">
+        <swiper-slide><img src="../assets/wmw_buenos_aires_1.jpg"></swiper-slide>
+        <swiper-slide><img src="../assets/wmw_buenos_aires_2.jpg"></swiper-slide>
+        <swiper-slide><img src="../assets/wmw_rio_1.jpg"></swiper-slide>
+      </swiper-container>
+</template>
   
   <script>
   export default {
